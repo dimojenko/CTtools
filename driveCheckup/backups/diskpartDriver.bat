3b0a79a7899044331cb6bd2a380fdb420285914c
dimojenko <dmojsejenko@gmail.com>
2024-04-22 17:11:25 -0400

    Add driveCheckup.

--- driveCheckup/backups/diskpartDriver.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /D "%~dp0"
-"C:\Users\efX-user\AppData\Local\Microsoft\WindowsApps\python.exe" "C:\Users\efX-user\Documents\driveCheckup\diskpart.py" > "C:\Users\efX-user\Documents\driveCheckup\diskpartTest.txt"
