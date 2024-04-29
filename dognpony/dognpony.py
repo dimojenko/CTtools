405cfc9f79877d215103a17dbd28377b710f1122
dimojenko <dmojsejenko@gmail.com>
2024-04-29 15:46:28 -0400

    Add nsivol cability in dognpony.

--- dognpony/dognpony.py
+++ dognpony/dognpony.py
@@ -10,7 +10,7 @@ configuration, simply delete any currentDesktop.txt in the run path and rerun.
 
 Requirements: 
     * Run in Windows shell
-    * Save files to display in "D:\5 - Tours\000000 OPEN HOUSE YO 000000\"
+    * Files will only be searched for in "D:\"
 
 Author: Dimitri Mojsejenko
 """
@@ -36,12 +36,11 @@ def saveWindows():
     with open(r'.\currentDesktop.txt', 'w', encoding='utf-8') as curDesktop:
         for window in win_list:
             print(window, file=curDesktop)
-            #curDesktop.write('%s\n' %window)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def findFile(name):
     fpath = False
-    for root, dirs, files in os.walk("D:\\5 - Tours\\000000 OPEN HOUSE YO 000000"):
+    for root, dirs, files in os.walk("D:"):
         if name in files:
             fpath = os.path.join(root, name)
     return fpath
@@ -56,13 +55,14 @@ def displayWindows():
                 windowPath = findFile(windowName)
                 if windowPath:
                     os.startfile(windowPath)
-            if ".nsihdr" in window:
-                windowName = window.split('.nsihdr')[0]+'.nsihdr'
-                windowName = windowName.split(' - ')[1]
-                windowLoc = window.split(', ')[2]
-                windowPathRoot = window.split(', ')[0].split(' (')[1].split(')')[0]
-                windowPath = windowPathRoot+'\\'+windowName
-                os.startfile(windowPath)
+            for fileExt in [".nsihdr", ".nsivol"]:
+                if fileExt in window:
+                    windowName = window.split(fileExt)[0]+fileExt
+                    windowName = " - ".join(windowName.split(' - ')[1::])
+                    windowLoc = window.split(', ')[2]
+                    windowPathRoot = window.split(', ')[0].split(' (')[1].split(')')[0]
+                    windowPath = windowPathRoot+'\\'+windowName
+                    os.startfile(windowPath)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def moveWindows():
@@ -70,42 +70,34 @@ def moveWindows():
     with open(r'.\currentDesktop.txt', 'r') as curDesktop:
         for line in curDesktop:
             savedWindows.append(line)
-    #print(savedWindows)
     time.sleep(30)
     openWindows = []
     win32gui.EnumWindows(getWindows, openWindows)
-    #print(openWindows)
     for window in openWindows:
         if ".jpg" in window:
-            #print(window)
             windowName = window.split('.jpg')[0]+'.jpg'
             windowHWND = window.split(', ')[1]
-            #print(windowHWND)
             for savedWindow in savedWindows:
                 if windowName in savedWindow:
                     windowLoc = savedWindow.split(', ')[2]
                     windowLoc = ''.join(c for c in windowLoc if c not in '[]\\n')
                     windowLoc = windowLoc.split(',')
-                    #print(windowLoc)
                     windowMoved = win32gui.MoveWindow(windowHWND, int(windowLoc[0]), int(windowLoc[1]), int(windowLoc[2])-int(windowLoc[0]), int(windowLoc[3])-int(windowLoc[1]), True)
-        if ".nsihdr" in window:
-            #print(window)
-            windowName = window.split('.nsihdr')[0]+'.nsihdr'
-            if "Resampling" in windowName:
-                windowName = windowName.rsplit('\\',1)[-1]
-            else:
-                windowName = windowName.split(' - ')[1]
-            #print(windowName)
-            windowHWND = window.split(', ')[1]
-            #print(windowHWND)
-            for savedWindow in savedWindows:
-                if windowName in savedWindow:
-                    if not "Resampling" in windowName:
-                        windowLoc = savedWindow.split(', ')[2]
-                        windowLoc = ''.join(c for c in windowLoc if c not in '[]\\n')
-                        windowLoc = windowLoc.split(',')
-                        #print(windowLoc)
-                        windowMoved = win32gui.MoveWindow(windowHWND, int(windowLoc[0]), int(windowLoc[1]), int(windowLoc[2])-int(windowLoc[0]), int(windowLoc[3])-int(windowLoc[1]), True)
+        for fileExt in [".nsihdr", ".nsivol"]:
+            if fileExt in window:
+                windowName = window.split(fileExt)[0]+fileExt
+                if "Resampling" in windowName:
+                    windowName = windowName.rsplit('\\',1)[-1]
+                else:
+                    windowName = windowName.split(' - ')[1]
+                windowHWND = window.split(', ')[1]
+                for savedWindow in savedWindows:
+                    if windowName in savedWindow:
+                        if not "Resampling" in windowName:
+                            windowLoc = savedWindow.split(', ')[2]
+                            windowLoc = ''.join(c for c in windowLoc if c not in '[]\\n')
+                            windowLoc = windowLoc.split(',')
+                            windowMoved = win32gui.MoveWindow(windowHWND, int(windowLoc[0]), int(windowLoc[1]), int(windowLoc[2])-int(windowLoc[0]), int(windowLoc[3])-int(windowLoc[1]), True)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if __name__ == '__main__':
